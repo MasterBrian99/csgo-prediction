392bb66551242a7b0672dffc2039747bfe82a05d
Pasindu P Konghawaththa <pasindudixon25@gmail.com>
2022-07-09 21:39:02 +0530

    update data scrape

--- data-collecter-js/lib/teamData.js
+++ data-collecter-js/lib/teamData.js
@@ -20,7 +20,7 @@ const csvWriter = createCsvWriter({
 });
 
 const csvPlayerWriter = createCsvWriter({
-  path: "playerData.csv",
+  path: "teamPlayerData.csv",
   append: true,
   header: [
     { id: "name", title: "name" },
@@ -30,6 +30,20 @@ const csvPlayerWriter = createCsvWriter({
     { id: "kd_difference", title: "kd_difference" },
     { id: "kd_ratio", title: "kd_ratio" },
     { id: "rating", title: "rating" },
+  ],
+});
+
+const csvTopMatch = createCsvWriter({
+  path: "teamTopMatchData.csv",
+  append: true,
+  header: [
+    { id: "team", title: "team" },
+    { id: "date", title: "date" },
+    { id: "opponent", title: "opponent" },
+    { id: "map", title: "map" },
+    { id: "w/l", title: "w/l" },
+    { id: "outcome", title: "outcome" },
+    { id: "year", title: "year" },
   ],
 });
 
@@ -46,9 +60,9 @@ const teamDataCsv = async () => {
       .replace("+", "");
     let kd_ratio = $(this).find("td:nth-child(4)").text();
     let rating = $(this).find("td:nth-child(5)").text();
-    let url =
-      "https://www.hltv.org" +
-      $(this).find("td.teamCol-teams-overview a").attr("href");
+    let url = generateUrl(
+      $(this).find("td.teamCol-teams-overview a").attr("href")
+    );
     const obj = {
       team_name,
       country,
@@ -65,13 +79,22 @@ const teamDataCsv = async () => {
     if (obj.team_name !== "TYLOO") {
       csvWriter.writeRecords(aaArr).then(() => {
         aaArr = [];
-        console.log("The CSV file was written successfully");
+        console.log(
+          `The CSV file was written successfully for ${obj.team_name}`
+        );
         let $ = cheerio.load(data);
       });
     }
   });
 };
 
+function generateUrl(url) {
+  const page_url = "https://www.hltv.org/stats";
+  const temp_arr = url.split("teams");
+  /* console.log(temp_arr) */
+  const new_url = page_url + "/teams/matches" + temp_arr[1];
+  return new_url;
+}
 const getTopPlayerList = async () => {
   const data = await fetchData(`https://www.hltv.org/stats/players`);
   let $ = cheerio.load(data);
@@ -116,7 +139,9 @@ const getTopPlayerList = async () => {
             // console.log(aaArr);
             csvPlayerWriter.writeRecords(aaArr).then(() => {
               aaArr = [];
-              console.log("The CSV file was written successfully");
+              console.log(
+                `The CSV file was written successfully for ${obj.team}`
+              );
             });
           }
         });
@@ -124,7 +149,67 @@ const getTopPlayerList = async () => {
     });
 };
 
+const topMatchList = async () => {
+  let count = 0;
+  let topTeamList = [];
+  fs.createReadStream("teamData.csv")
+    .pipe(csv())
+    .on("data", (row) => {
+      if (count <= 4) {
+        topTeamList.push(row.url);
+        // console.log(row.url);
+        // console.log(row.team_name);
+        topMatch(row.url, row.team_name);
+      }
+      count++;
+    })
+    .on("end", () => {
+      console.log("asd");
+    });
+};
+
+const topMatch = async (url, team) => {
+  if (url == undefined) {
+    return;
+  }
+  console.log(url);
+  const data = await fetchData(url);
+  let $ = cheerio.load(data);
+  $(
+    "html body.colsCustom1101 div.bgPadding div.widthControl div.colCon div.contentCol div.stats-section.stats-team.stats-team-matches table.stats-table.no-sort tbody tr"
+  ).each(function (i, ele) {
+    let date = $(this).find("td:nth-child(1) > a:nth-child(1)").text();
+    let opponent = $(this).find("td:nth-child(4) > a:nth-child(1)").text();
+    let map = $(this).find("td:nth-child(5) > span:nth-child(1)").text();
+    let win_lose = $(this)
+      .find("td:nth-child(6) > span:nth-child(1)")
+      .text()
+      .replace(/ /g, "");
+    let outcome = $(this).find("td:nth-child(7)").text() == "W" ? "1" : "0";
+    let year =
+      "20" +
+      $(this).find("td:nth-child(1) > a:nth-child(1)").text().split("/")[2];
+    const obj = {
+      team,
+      date,
+      opponent,
+      map,
+      win_lose,
+      outcome,
+      year,
+    };
+    let aaArr = [];
+    aaArr.push(obj);
+    // console.log(aaArr);
+    csvTopMatch.writeRecords(aaArr).then(() => {
+      aaArr = [];
+      console.log(`The CSV file was written successfully for ${obj.team}`);
+    });
+  });
+};
+
 module.exports = {
   teamDataCsv,
   getTopPlayerList,
+  topMatchList,
 };
--- data-collecter-js/server.js
+++ data-collecter-js/server.js
@@ -1,14 +1,81 @@
+const createCsvWriter = require("csv-writer").createObjectCsvWriter;
+const csv = require("csv-parser");
+const fs = require("fs");
 const { dailyData, playerData } = require("./lib/playerData");
-const { teamDataCsv, getTopPlayerList } = require("./lib/teamData");
+const {
+  teamDataCsv,
+  getTopPlayerList,
+  topMatchList,
+} = require("./lib/teamData");
 
-// let count = 100;
-// while (count < 10000) {
-//   dailyData(count);
-//   count = count + 100;
-// }
+const oldPaths = ["teamData.csv", "teamPlayerData.csv", "teamMatch.csv"];
+
+const teamDataCSV = createCsvWriter({
+  path: "teamData.csv",
+  header: [
+    { id: "team_name", title: "team_name" },
+    { id: "country", title: "country" },
+    { id: "maps_played", title: "maps_played" },
+    { id: "kd_difference", title: "kd_difference" },
+    { id: "kd_ratio", title: "kd_ratio" },
+    { id: "rating", title: "rating" },
+    { id: "url", title: "url" },
+  ],
+});
+
+const teamPlayerDataCSV = createCsvWriter({
+  path: "teamPlayerData.csv",
+  header: [
+    { id: "name", title: "name" },
+    { id: "team", title: "team" },
+    { id: "maps_played", title: "maps_played" },
+    { id: "rounds_played", title: "rounds_played" },
+    { id: "kd_difference", title: "kd_difference" },
+    { id: "kd_ratio", title: "kd_ratio" },
+    { id: "rating", title: "rating" },
+  ],
+});
+
+const csvTopMatch = createCsvWriter({
+  path: "teamTopMatchData.csv",
+  header: [
+    { id: "team", title: "team" },
+    { id: "date", title: "date" },
+    { id: "opponent", title: "opponent" },
+    { id: "map", title: "map" },
+    { id: "win_lose", title: "win_lose" },
+    { id: "outcome", title: "outcome" },
+    { id: "year", title: "year" },
+  ],
+});
 
 function getData() {
-  teamDataCsv();
+  oldPaths.forEach((e) => {
+    try {
+      fs.unlinkSync(e);
+      //file removed
+    } catch (err) {
+      console.error("file not found !");
+    }
+  });
+  teamDataCSV.writeRecords([]).then(() => {
+    console.log("CSV file created");
+  });
+  teamPlayerDataCSV.writeRecords([]).then(() => {
+    console.log("CSV file created");
+  });
+  csvTopMatch.writeRecords([]).then(() => {
+    console.log("CSV file created");
+  });
+  teamDataCsv()
+    .then(() => {
+      getTopPlayerList().then(() => {
+        topMatchList();
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 }
 
 getData();
